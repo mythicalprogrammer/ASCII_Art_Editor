267b5d76b84f03231d768da0c7635ee67098217a
Anthony Doan <mythicalprogrammer@gmail.com>
2013-03-30 13:45:24 -0700

    finished tutorial part 1

---
+++ src/com/example/tuts_plus_ascii_art_editor_android/ColorChooser.java
@@ -0,0 +1,14 @@
+package com.example.tuts_plus_ascii_art_editor_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ColorChooser extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.color_choice);
+	}
+
+}
