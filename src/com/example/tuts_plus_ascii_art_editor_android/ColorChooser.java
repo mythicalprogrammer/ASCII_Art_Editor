cc2c8f907759bc74a9dbdbe081c288fc04353648
Anthony Doan <mythicalprogrammer@gmail.com>
2013-03-30 20:59:43 -0700

    finished part II but it's not working

--- src/com/example/tuts_plus_ascii_art_editor_android/ColorChooser.java
+++ src/com/example/tuts_plus_ascii_art_editor_android/ColorChooser.java
@@ -1,7 +1,9 @@
 package com.example.tuts_plus_ascii_art_editor_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ColorChooser extends Activity {
 
@@ -9,6 +11,21 @@ public class ColorChooser extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.color_choice);
+	}
+	
+	public void setColors(View view) {
+		String tagInfo = (String)view.getTag();
+		String[] tagColors = tagInfo.split(" ");
+		/* Now we want to pass this data back to the main Activity 
+		  so that we can apply the color scheme settings to the 
+		  user interface elements there. To do this we use an Intent */
+		Intent backIntent = new Intent();
+		backIntent.putExtra("textColor", tagColors[0]);
+		backIntent.putExtra("backColor", tagColors[1]);
+		// set  the return intent result
+		setResult(RESULT_OK, backIntent);
+		//return to the activity that called this one by finishing:
+		finish();
 	}
 
 }
